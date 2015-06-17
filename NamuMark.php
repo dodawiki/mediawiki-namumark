c93749f9eec7b8e20dfd383ee1476dfaf16e0f75
KimDongDong <Oriwiki@users.noreply.github.com>
2015-06-18 07:46:00 +0900

    add mediawiki extension config file

---
+++ NamuMark.php
@@ -0,0 +1,46 @@
+<?php
+ 
+// Take credit for your work.
+$wgExtensionCredits['parserhook'][] = array(
+ 
+   // The full path and filename of the file. This allows MediaWiki
+   // to display the Subversion revision number on Special:Version.
+   'path' => __FILE__,
+ 
+   // The name of the extension, which will appear on Special:Version.
+   'name' => '나무마크 미디어위키판',
+ 
+   // A description of the extension, which will appear on Special:Version.
+   'description' => 'PHP 나무마크를 미디어위키에 적용합니다.',
+ 
+   // Alternatively, you can specify a message key for the description.
+  // 'descriptionmsg' => 'exampleextension-desc',
+ 
+   // The version of the extension, which will appear on Special:Version.
+   // This can be a number or a string.
+   'version' => '0.1', 
+ 
+   // Your name, which will appear on Special:Version.
+   'author' => 'koreapyj 원본, 김동동 수정',
+ 
+   // The URL to a wiki page/web page with information about the extension,
+   // which will appear on Special:Version.
+   'url' => 'https://github.com/koreapyj/php-namumark',
+ 
+);
+$wgHooks['OutputPageBeforeHTML'][] = 'NamuMark';
+
+
+
+function NamuMark( OutputPage &$out, &$text ) { 
+	global $namu_articepath;
+	require_once("namumark.php");
+	$wPage = new PlainWikiPage("$text");
+	$wEngine = new NamuMark($wPage);
+	$wEngine->prefix = "$namu_articepath";
+	$text =  $wEngine->toHtml();
+	
+	}
+
+	
+?>
