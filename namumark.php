77a03dc66fef224718c29f5f8d6bebc48b13357f
KimDongDong <Oriwiki@users.noreply.github.com>
2015-08-16 06:02:32 +0900

    Fix bug of table

--- namumark.php
+++ namumark.php
@@ -74,7 +74,7 @@ function NamuMark(&$parser, &$text, &$strip_state) {
 		
 		$text = preg_replace('/src="http:\/\/www\.youtube\.com/', 'src="//www.youtube.com', $text); // HHTPS 환경에서 비 SSL 유튜브 URL 첨부시 재생이 안 되는 버그를 위해 SSL URL로 변경.
 		
-		$text = preg_replace('/^(\|\|<table .*?>)(\|\|+)/im', '$2$1', $text);
+		$text = preg_replace('/^(\|\|<table.*?>)(\|\|+)/im', '$2$1', $text);
 		
 		# 파서를 불러온다.
 		require_once("php-namumark.php");
