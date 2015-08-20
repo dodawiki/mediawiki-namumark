483ac6f9cf1ca06824cb0c050b3bd9885e90b7dd
KimDongDong <Oriwiki@users.noreply.github.com>
2015-08-20 14:48:13 +0900

    Modify version

--- namumark.php
+++ namumark.php
@@ -18,7 +18,7 @@ $wgExtensionCredits['parserhook'][] = array(
  
    // The version of the extension, which will appear on Special:Version.
    // This can be a number or a string.
-   'version' => '0.4.3.1', 
+   'version' => '0.4.4', 
  
    // Your name, which will appear on Special:Version.
    'author' => 'koreapyj 원본, 김동동 수정',
