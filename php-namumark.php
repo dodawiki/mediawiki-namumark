6f34edc1873ab152757e27a4604fd557b09f8e02
KimDongDong <Oriwiki@users.noreply.github.com>
2015-08-24 10:17:32 +0900

    Fix bug of table

--- php-namumark.php
+++ php-namumark.php
@@ -183,7 +183,7 @@ class NamuMark {
 				continue;
 			}
 
-			/*
+			
 			if(self::startsWith($text, '|', $i) && $table = $this->tableParser($text, $i)) {
 				$result .= ''
 					.$table
@@ -192,7 +192,7 @@ class NamuMark {
 				$now = '';
 				continue;
 			}
-*/
+
 			if($line == '' && self::startsWith($text, '>', $i) && $blockquote = $this->bqParser($text, $i)) {
 				$result .= ''
 					.$blockquote
