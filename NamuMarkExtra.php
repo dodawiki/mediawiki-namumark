47849777a0e1ad7a0a9dad83932bbbd037bda27a
KimDongDong <contact@oriwiki.net>
2015-11-10 16:29:50 +0900

    Add checking array

--- NamuMarkExtra.php
+++ NamuMarkExtra.php
@@ -105,16 +105,18 @@ class NamuMarkExtra {
     private function printTemplateParameter($text) {
         $properties = $GLOBALS['template'];
         $i = 1;
-        foreach($properties as $property) {
-            if($property == $properties[0])
-                continue;
-            $property = explode('=', $property);
-            $property[0] = trim($property[0]);
-            if(isset($property[1])) {
-                $text = str_replace('@'.$property[0].'@', $property[1], $text);
-            } else {
-                $text = str_replace('@'.$i.'@', $property[0], $text);
-                $i++;
+		if (is_array($properties)) {
+            foreach ($properties as $property) {
+                if ($property == $properties[0])
+                    continue;
+                $property = explode('=', $property);
+                $property[0] = trim($property[0]);
+                if (isset($property[1])) {
+                    $text = str_replace('@' . $property[0] . '@', $property[1], $text);
+                } else {
+                    $text = str_replace('@' . $i . '@', $property[0], $text);
+                    $i++;
+                }
             }
         }
 
