280122b11fa25f19e1dc6523bea81d22eb18e769
KimDongDong <contact@oriwiki.net>
2015-11-12 17:57:55 +0900

    Version up

--- namumark.php
+++ namumark.php
@@ -3,33 +3,31 @@
 // Take credit for your work.
 $wgExtensionCredits['parserhook'][] = array(
  
-   // The full path and filename of the file. This allows MediaWiki
-   // to display the Subversion revision number on Special:Version.
-   'path' => __FILE__,
+	// The full path and filename of the file. This allows MediaWiki
+	// to display the Subversion revision number on Special:Version.
+	'path' => __FILE__,
  
-   // The name of the extension, which will appear on Special:Version.
-   'name' => '나무마크 미디어위키판',
+	// The name of the extension, which will appear on Special:Version.
+	'name' => '나무마크 미디어위키판',
  
-   // A description of the extension, which will appear on Special:Version.
-   'description' => 'PHP 나무마크를 미디어위키에 적용합니다.',
+	// A description of the extension, which will appear on Special:Version.
+	'description' => 'PHP 나무마크를 미디어위키에 적용합니다.',
+
+	// The version of the extension, which will appear on Special:Version.
+	// This can be a number or a string.
+	'version' => '1.0.6',
  
-   // Alternatively, you can specify a message key for the description.
-  // 'descriptionmsg' => 'exampleextension-desc',
+	// Your name, which will appear on Special:Version.
+	'author' => 'koreapyj 원본, 김동동 수정',
  
-   // The version of the extension, which will appear on Special:Version.
-   // This can be a number or a string.
-   'version' => '1.0.5',
- 
-   // Your name, which will appear on Special:Version.
-   'author' => 'koreapyj 원본, 김동동 수정',
- 
-   // The URL to a wiki page/web page with information about the extension,
-   // which will appear on Special:Version.
-   'url' => 'https://github.com/Oriwiki/php-namumark-mediawiki',
+	// The URL to a wiki page/web page with information about the extension,
+	// which will appear on Special:Version.
+	'url' => 'https://github.com/Oriwiki/php-namumark-mediawiki',
    
-'license-name' => "AGPL-3.0",   // Short name of the license, links LICENSE or COPYING file if existing - string, added in 1.23.0
+	'license-name' => "AGPL-3.0",   // Short name of the license, links LICENSE or COPYING file if existing - string, added in 1.23.0
   
 );
+
 $wgHooks['ParserBeforeStrip'][] = 'NamuMark';
 $wgHooks['InternalParseBeforeLinks'][] = 'NamuMarkHTML';
 $wgHooks['ParserBeforeTidy'][] = 'NamuMarkHTML2';
