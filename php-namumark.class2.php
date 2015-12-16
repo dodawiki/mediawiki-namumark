556137604ae04923060952c4cf746b155a57f893
KimDongDong <contact@oriwiki.net>
2015-12-16 16:48:53 +0900

    Fix external image on table bug

--- php-namumark.class2.php
+++ php-namumark.class2.php
@@ -155,9 +155,8 @@ class NamuMark2 extends NamuMark {
 			$block = $this->blockParser($match[5]);
 		}
 
-		if(preg_match('/^(.*?)(?<!<nowiki>)(https?.*?)(\.jpeg|\.jpg|\.png|\.gif)([?&][^< ]+)(?!<\/nowiki>)(.*)$/i', $block, $match)) {
-			$vowels = array('?', '&');
-			$match[4] = str_replace($vowels, ' ', $match[4]);
+		if(preg_match('/^(.*?)(?<!<nowiki>)(https?[^<]*?)(\.jpeg|\.jpg|\.png|\.gif)([?&][^< ]+)(?!<\/nowiki>)(.*)$/i', $block, $match)) {
+			$match[4] = str_replace(array('?', '&'), ' ', $match[4]);
 
 			$match[4] = str_ireplace('align=left', '', $match[4]);
 			$match[4] = str_ireplace('align=center', 'style="display:block; margin-left:auto; margin-right:auto;"', $match[4]);
