2bbd39c4e78f09883978e386e5614d1182817f59
ddarkr <admin@ddark.kr>
2020-03-10 00:40:36 +0900

    fix: 🔥 Folding 이 2개 이상이면 안되는 문제 해결

--- resources/init.js
+++ resources/init.js
@@ -1,8 +1,11 @@
-document.querySelector('.nm-folding .text').addEventListener('click', function(event) {
-  var content = event.target.parentNode.querySelector('.folding-content');
-  if (content.style.display === 'block') {
-  	content.style.display = 'none';
-  } else {
-  	content.style.display = 'block';
-  }
-})
+var foldingItemList = document.querySelectorAll('.nm-folding .text');
+for ( var i = 0; i < foldingItemList.length; i++ ) {
+  foldingItemList[i].addEventListener('click', function(event) {
+    var content = event.target.parentNode.querySelector('.folding-content');
+    if (content.style.display === 'block') {
+    	content.style.display = 'none';
+    } else {
+    	content.style.display = 'block';
+    }
+  })
+}
