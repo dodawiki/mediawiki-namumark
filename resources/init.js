c75aacbe0f5c7d12c5d23b3e21d3ba7f208fcb68
도다 <ddark.kr@gmail.com>
2021-02-28 18:34:29 +0900

    chore: 확장기능 정보 새로 씀 (#1)

--- resources/init.js
+++ resources/init.js
@@ -1,11 +1,11 @@
-var foldingItemList = document.querySelectorAll('.nm-folding .text');
-for ( var i = 0; i < foldingItemList.length; i++ ) {
-  foldingItemList[i].addEventListener('click', function(event) {
-    var content = event.target.parentNode.querySelector('.folding-content');
-    if (content.style.display === 'block') {
-    	content.style.display = 'none';
+const foldingItemList = document.querySelectorAll(".nm-folding .text");
+for (let i = 0; i < foldingItemList.length; i++) {
+  foldingItemList[i].addEventListener("click", function (event) {
+    const content = event.target.parentNode.querySelector(".folding-content");
+    if (content.style.display === "block") {
+      content.style.display = "none";
     } else {
-    	content.style.display = 'block';
+      content.style.display = "block";
     }
-  })
+  });
 }
