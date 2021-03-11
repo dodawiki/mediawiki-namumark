8ed509f2c495ff491ed126cd44a84676edb9eee8
ddark <ddark.kr@gmail.com>
2021-03-12 00:59:16 +0900

    feat: 레거시 전환 및 class 이름 변경

---
+++ includes/NamuMarkExtension.php
@@ -0,0 +1,107 @@
+<?php
+
+class NamuMarkExtension
+{
+	public static function onBeforePageDisplay()
+	{
+		global $wgOut, $wgScriptPath;
+
+		$wgOut->addModules('ext.NamuMark');
+	}
+
+	public static function markParse(Parser &$parser, &$text)
+	{
+		$title = $parser->getTitle();
+
+		# 상기의 확인 함수의 반환값과, 현 URI가 히스토리인지 확인하는 함수의 반환값과, 현 문서가 특수:기여 또는 특수:기록인지 확인하는 함수의 반환값을 확인한다.
+		if (!preg_match('/^특수:/', $title) && !preg_match("/&action=history/", $_SERVER["REQUEST_URI"]) && !preg_match('/^사용자:.*\.(css|js)$/', $title)) {
+			$text = html_entity_decode($text, ENT_QUOTES | ENT_HTML5); // HTML 엔티티를 디코드한다.
+
+			# '[[내부 링크|<span style="color:색깔값">표시내용<span>]]'와 같은 내부 링크 글씨의 색깔을 지정하는 방식이 버그를 일으키므로
+			# 미디어위키에서 지원하는 글씨 색 방식으로 바꾼다.
+			$text = preg_replace('/<span style="color:(.*?)">(.*?)<\/span>\]\]/i', '{{글씨 색|$1|$2}}]]', $text);
+			$text = preg_replace('/<font color="(.*?)">(.*?)<\/font>\]\]/i', '{{글씨 색|$1|$2}}]]', $text);
+
+			# 문서 구판에 접속시 최상단의 코드를 별도의 변수로 일단 보관하고 제거한다. 파서에 적용되지 않도록 하기 위함. 문서 구판에 접속시 발생하는 버그로 인한 조치.
+			if (preg_match('/&oldid=/', $_SERVER["REQUEST_URI"])) {
+				preg_match('/^.*$/m', $text, $fn);
+				$text = str_replace("$fn[0]", '', $text);
+			}
+
+			$text = preg_replace('/<pre .*?>(.*?)<\/pre>/s', '<pre>$1</pre>', $text);
+
+			# 보조 파서를 불러온다.
+			$Extra = new NamuMarkExtra($text, $title);
+			$Extra->title();
+			$mediawikiTable = $Extra->cutMediawikiTable();
+			$Extra->table();
+			$Extra->indent();
+			$Extra->getTemplateParameter();
+			$text = $Extra->text;
+
+			# 파서를 불러온다.
+			$wEngine = new NamuMarkExtended($text, $title);
+			$text = $wEngine->toHtml();
+
+			# 상기에서 볃도로 보관한 변수의 값을 본문의 바로 앞에 추가한다.
+			if (preg_match('/&oldid=/', $_SERVER["REQUEST_URI"])) {
+				$text = $fn[0] . $text;
+			}
+
+			preg_match_all('/<html>(.*?)<\/html>/s', $text, $html);
+
+			foreach ($html[1] as $code) {
+				$lines = explode("\n", $code);
+				$code_ex = '';
+				foreach ($lines as $key => $line) {
+					if ((!$key && !$lines[$key]) || ($key == count($lines) - 1 && !$lines[$key]))
+						continue;
+					if (preg_match('/^(:+)/', $line, $match)) {
+						$line = substr($line, strlen($match[1]));
+						$add = '';
+						for ($i = 1; $i <= strlen($match[1]); $i++)
+							$add .= ' ';
+						$line = $add . $line;
+						$code_ex .= $line . "\n";
+					} else {
+						if (!isset($lines[$key + 1]) || $lines[$key + 1] === '')
+							$code_ex .= $line;
+						else
+							$code_ex .= $line . "\n";
+					}
+				}
+				$xss = new XssHtml($code_ex);
+				$text = str_replace($code, $xss->getHtml(), $text);
+			}
+
+			$Extra = new NamuMarkExtra($text, $title);
+			$Extra->pasteMediawikiTable($mediawikiTable);
+			$text = $Extra->text;
+		}
+	}
+
+	public static function markHandleHTML(&$parser, &$text)
+	{
+		$title = $parser->getTitle();
+
+		if (!preg_match('/^특수:/', $title) && !preg_match("/&action=history/", $_SERVER["REQUEST_URI"]) && !preg_match('/^사용자:.*\.(css|js)$/', $title)) {
+			$Extra = new NamuMarkExtra($text, $title);
+			preg_match('/(<div id="specialchars".*<\/div>)/s', $text, $charinsert);
+			$text = preg_replace('/(<div id="specialchars".*<\/div>)/s', '', $text);
+
+			$text = str_replace("<br /></p>\n<p>", '<br />', $text);
+			$text = str_replace("<p><br />\n</p>", '', $text);
+
+			$text = preg_replace('/<a rel="nofollow" target="_blank" class="external autonumber" href="(.*?)">\[(\[\d+\])\]<\/a>/',
+				'<a rel="nofollow" target="_blank" class="external autonumber" href="$1">$2</a>',
+				$text);
+
+			$text = preg_replace('@^<ol><li><ol><li>.*?</li></ol></li></ol>$@ms', '', $text);
+
+			$Extra->external();
+			$Extra->imageurl();
+			$Extra->printTemplateParameter();
+			$text = $Extra->text;
+		}
+	}
+}
--- includes/XSSfilter.php
+++ includes/XSSfilter.php
@@ -1,14 +1,14 @@
 <?php
 /**
-* PHP 富文本XSS???
-*
-* @package XssHtml
-* @version 1.0.0
-* @link http://phith0n.github.io/XssHtml
-* @since 20140621
-* @copyright (c) Phithon All Rights Reserved
-*
-*/
+ * PHP 富文本XSS???
+ *
+ * @package XssHtml
+ * @version 1.0.0
+ * @link http://phith0n.github.io/XssHtml
+ * @since 20140621
+ * @copyright (c) Phithon All Rights Reserved
+ *
+ */
 
 #
 # Written by Phithon <root@leavesongs.com> in 2014 and placed in
@@ -29,200 +29,207 @@
 # ??器版本：IE7+ 或其他??器，无法防御IE6及以下版本??器中的XSS
 # 更多使用??? http://phith0n.github.io/XssHtml
 
-class XssHtml {
-    private $m_dom;
-    private $m_xss;
-    private $m_ok;
-    private $m_AllowAttr = array('title', 'src', 'href', 'id', 'class', 'style', 'width', 'height', 'alt', 'target', 'align', 'allowfullscreen', 'frameborder');
-    private $m_AllowTag = array('a', 'img', 'br', 'strong', 'b', 'code', 'pre', 'p', 'div', 'em', 'span', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'table', 'ul', 'ol', 'tr', 'th', 'td', 'hr', 'li', 'u', 'iframe', 'embed');
+class XssHtml
+{
+	private $m_dom;
+	private $m_xss;
+	private $m_ok;
+	private $m_AllowAttr = array('title', 'src', 'href', 'id', 'class', 'style', 'width', 'height', 'alt', 'target', 'align', 'allowfullscreen', 'frameborder');
+	private $m_AllowTag = array('a', 'img', 'br', 'strong', 'b', 'code', 'pre', 'p', 'div', 'em', 'span', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'table', 'ul', 'ol', 'tr', 'th', 'td', 'hr', 'li', 'u', 'iframe', 'embed');
 
-    /**
-    * ?造函?
-    *
-    * @param string $html 待??的文本
-    * @param string $charset 文本??，默?utf-8
-    * @param array $AllowTag 允?的??，如果不?楚?保持默?，默?已涵盖大部分功能，不要增加危???
-    */
-    public function __construct($html, $charset = 'utf-8', $AllowTag = array()){
-        $this->m_AllowTag = empty($AllowTag) ? $this->m_AllowTag : $AllowTag;
-        $this->m_xss = strip_tags($html, '<' . implode('><', $this->m_AllowTag) . '>');
-        if (empty($this->m_xss)) {
-            $this->m_ok = FALSE;
-            return;
-        }
-        $this->m_xss = "<meta http-equiv=\"Content-Type\" content=\"text/html;charset={$charset}\"><nouse>" . $this->m_xss . "</nouse>";
-        $this->m_dom = new DOMDocument();
-        $this->m_dom->strictErrorChecking = FALSE;
-        $this->m_ok = @$this->m_dom->loadHTML($this->m_xss);
-    }
+	/**
+	 * ?造函?
+	 *
+	 * @param string $html 待??的文本
+	 * @param string $charset 文本??，默?utf-8
+	 * @param array $AllowTag 允?的??，如果不?楚?保持默?，默?已涵盖大部分功能，不要增加危???
+	 */
+	public function __construct($html, $charset = 'utf-8', $AllowTag = array())
+	{
+		$this->m_AllowTag = empty($AllowTag) ? $this->m_AllowTag : $AllowTag;
+		$this->m_xss = strip_tags($html, '<' . implode('><', $this->m_AllowTag) . '>');
+		if (empty($this->m_xss)) {
+			$this->m_ok = FALSE;
+			return;
+		}
+		$this->m_xss = "<meta http-equiv=\"Content-Type\" content=\"text/html;charset={$charset}\"><nouse>" . $this->m_xss . "</nouse>";
+		$this->m_dom = new DOMDocument();
+		$this->m_dom->strictErrorChecking = FALSE;
+		$this->m_ok = @$this->m_dom->loadHTML($this->m_xss);
+	}
 
-    /**
-    * ?得??后的?容
-    */
-    public function getHtml()
-    {
-      if (!$this->m_ok) {
-         return '';
-      }
-      $nodeList = $this->m_dom->getElementsByTagName('*');
-      for ($i = 0; $i < $nodeList->length; $i++){
-         $node = $nodeList->item($i);
-         if (in_array($node->nodeName, $this->m_AllowTag)) {
-            if (method_exists($this, "__node_{$node->nodeName}")) {
-               call_user_func(array($this, "__node_{$node->nodeName}"), $node);
-            }else{
-               call_user_func(array($this, '__node_default'), $node);
-            }
-         }
-      }
-      $html = strip_tags($this->m_dom->saveHTML(), '<' . implode('><', $this->m_AllowTag) . '>');
-      $html = preg_replace('/^\n(.*)\n$/s', '$1', $html);
-      return $html;
-    }
+	/**
+	 * ?得??后的?容
+	 */
+	public function getHtml()
+	{
+		if (!$this->m_ok) {
+			return '';
+		}
+		$nodeList = $this->m_dom->getElementsByTagName('*');
+		for ($i = 0; $i < $nodeList->length; $i++) {
+			$node = $nodeList->item($i);
+			if (in_array($node->nodeName, $this->m_AllowTag)) {
+				if (method_exists($this, "__node_{$node->nodeName}")) {
+					call_user_func(array($this, "__node_{$node->nodeName}"), $node);
+				} else {
+					call_user_func(array($this, '__node_default'), $node);
+				}
+			}
+		}
+		$html = strip_tags($this->m_dom->saveHTML(), '<' . implode('><', $this->m_AllowTag) . '>');
+		$html = preg_replace('/^\n(.*)\n$/s', '$1', $html);
+		return $html;
+	}
 
-    private function __true_url($url){
-      if (preg_match('#^(https?:)?//.+#is', $url)) {
-         return $url;
-      }else{
-         return 'http://' . $url;
-      }
-    }
+	private function __true_url($url)
+	{
+		if (preg_match('#^(https?:)?//.+#is', $url)) {
+			return $url;
+		} else {
+			return 'http://' . $url;
+		}
+	}
 
-    private function __get_style($node){
-      if ($node->attributes->getNamedItem('style')) {
-         $style = $node->attributes->getNamedItem('style')->nodeValue;
-         $style = str_replace('\\', ' ', $style);
-         $style = str_replace(array('&#', '/*', '*/'), ' ', $style);
-         $style = preg_replace('#e.*x.*p.*r.*e.*s.*s.*i.*o.*n#Uis', ' ', $style);
-         return $style;
-      }else{
-         return '';
-      }
-    }
+	private function __get_style($node)
+	{
+		if ($node->attributes->getNamedItem('style')) {
+			$style = $node->attributes->getNamedItem('style')->nodeValue;
+			$style = str_replace('\\', ' ', $style);
+			$style = str_replace(array('&#', '/*', '*/'), ' ', $style);
+			$style = preg_replace('#e.*x.*p.*r.*e.*s.*s.*i.*o.*n#Uis', ' ', $style);
+			return $style;
+		} else {
+			return '';
+		}
+	}
 
-    private function __get_link($node, $att){
-      $link = $node->attributes->getNamedItem($att);
-      if ($link) {
-         return $this->__true_url($link->nodeValue);
-      }else{
-         return '';
-      }
-    }
+	private function __get_link($node, $att)
+	{
+		$link = $node->attributes->getNamedItem($att);
+		if ($link) {
+			return $this->__true_url($link->nodeValue);
+		} else {
+			return '';
+		}
+	}
 
-    private function __setAttr($dom, $attr, $val){
-      if (!empty($val)) {
-         $dom->setAttribute($attr, $val);
-      }
-    }
+	private function __setAttr($dom, $attr, $val)
+	{
+		if (!empty($val)) {
+			$dom->setAttribute($attr, $val);
+		}
+	}
 
-    private function __setName(DOMNode $oldNode, $newName, $newNS = null) {
-        if (isset($newNS))
-        {
-            $newNode = $oldNode->ownerDocument->createElementNS($newNS, $newName);
-        }
-        else
-        {
-            $newNode = $oldNode->ownerDocument->createElement($newName);
-        }
+	private function __setName(DOMNode $oldNode, $newName, $newNS = null)
+	{
+		if (isset($newNS)) {
+			$newNode = $oldNode->ownerDocument->createElementNS($newNS, $newName);
+		} else {
+			$newNode = $oldNode->ownerDocument->createElement($newName);
+		}
 
-        foreach ($oldNode->attributes as $attr)
-        {
-            $newNode->appendChild($attr->cloneNode());
-        }
+		foreach ($oldNode->attributes as $attr) {
+			$newNode->appendChild($attr->cloneNode());
+		}
 
-        foreach ($oldNode->childNodes as $child)
-        {
-            $newNode->appendChild($child->cloneNode(true));
-        }
+		foreach ($oldNode->childNodes as $child) {
+			$newNode->appendChild($child->cloneNode(true));
+		}
 
-        $oldNode->parentNode->replaceChild($newNode, $oldNode);
-    }
+		$oldNode->parentNode->replaceChild($newNode, $oldNode);
+	}
 
-    private function __set_default_attr($node, $attr, $default = '')
-    {
-      $o = $node->attributes->getNamedItem($attr);
-      if ($o) {
-         $this->__setAttr($node, $attr, $o->nodeValue);
-      }else{
-         $this->__setAttr($node, $attr, $default);
-      }
-    }
+	private function __set_default_attr($node, $attr, $default = '')
+	{
+		$o = $node->attributes->getNamedItem($attr);
+		if ($o) {
+			$this->__setAttr($node, $attr, $o->nodeValue);
+		} else {
+			$this->__setAttr($node, $attr, $default);
+		}
+	}
 
-    private function __common_attr($node)
-    {
-      $list = array();
-      foreach ($node->attributes as $attr) {
-         if (!in_array($attr->nodeName,
-             $this->m_AllowAttr)) {
-            $list[] = $attr->nodeName;
-         }
-      }
-      foreach ($list as $attr) {
-         $node->removeAttribute($attr);
-      }
-      $style = $this->__get_style($node);
-      $this->__setAttr($node, 'style', $style);
-      $this->__set_default_attr($node, 'title');
-      $this->__set_default_attr($node, 'id');
-      $this->__set_default_attr($node, 'class');
-    }
+	private function __common_attr($node)
+	{
+		$list = array();
+		foreach ($node->attributes as $attr) {
+			if (!in_array($attr->nodeName,
+				$this->m_AllowAttr)) {
+				$list[] = $attr->nodeName;
+			}
+		}
+		foreach ($list as $attr) {
+			$node->removeAttribute($attr);
+		}
+		$style = $this->__get_style($node);
+		$this->__setAttr($node, 'style', $style);
+		$this->__set_default_attr($node, 'title');
+		$this->__set_default_attr($node, 'id');
+		$this->__set_default_attr($node, 'class');
+	}
 
-    private function __node_img($node){
-      $this->__common_attr($node);
+	private function __node_img($node)
+	{
+		$this->__common_attr($node);
 
-      $this->__set_default_attr($node, 'src');
-      $this->__set_default_attr($node, 'width');
-      $this->__set_default_attr($node, 'height');
-      $this->__set_default_attr($node, 'alt');
-      $this->__set_default_attr($node, 'align');
+		$this->__set_default_attr($node, 'src');
+		$this->__set_default_attr($node, 'width');
+		$this->__set_default_attr($node, 'height');
+		$this->__set_default_attr($node, 'alt');
+		$this->__set_default_attr($node, 'align');
 
-    }
+	}
 
-    private function __node_a($node){
-      $this->__common_attr($node);
-      $href = $this->__get_link($node, 'href');
+	private function __node_a($node)
+	{
+		$this->__common_attr($node);
+		$href = $this->__get_link($node, 'href');
 
-      $this->__setAttr($node, 'href', $href);
-      $this->__set_default_attr($node, 'target', '_blank');
-    }
+		$this->__setAttr($node, 'href', $href);
+		$this->__set_default_attr($node, 'target', '_blank');
+	}
 
-    private function __node_embed($node){
-        $this->__common_attr($node);
-        $link = $this->__get_link($node, 'src');
+	private function __node_embed($node)
+	{
+		$this->__common_attr($node);
+		$link = $this->__get_link($node, 'src');
 
-        if(preg_match('@^(?:https?:)?//www\.youtube\.com/v/(.*)@', $link, $youtube)) {
-            $link = '//www.youtube.com/embed/'.$youtube[1];
-            $this->__setAttr($node, 'src', $link);
-            $this->__setName($node, 'iframe');
-            return;
-        }
+		if (preg_match('@^(?:https?:)?//www\.youtube\.com/v/(.*)@', $link, $youtube)) {
+			$link = '//www.youtube.com/embed/' . $youtube[1];
+			$this->__setAttr($node, 'src', $link);
+			$this->__setName($node, 'iframe');
+			return;
+		}
 
-        $this->__setAttr($node, 'src', $link);
-        $this->__setAttr($node, 'allowscriptaccess', 'never');
-        $this->__set_default_attr($node, 'width');
-        $this->__set_default_attr($node, 'height');
-    }
+		$this->__setAttr($node, 'src', $link);
+		$this->__setAttr($node, 'allowscriptaccess', 'never');
+		$this->__set_default_attr($node, 'width');
+		$this->__set_default_attr($node, 'height');
+	}
 
-    private function __node_iframe($node) {
-        $this->__common_attr($node);
-        $link = $this->__get_link($node, 'src');
+	private function __node_iframe($node)
+	{
+		$this->__common_attr($node);
+		$link = $this->__get_link($node, 'src');
 
-        $link = preg_replace('@^http://www\.youtube\.com@', '//www.youtube.com', $link);
-        $link = preg_replace('@^http://videofarm\.daum\.net@', '//videofarm.daum.net', $link);
-        $link = preg_replace('@^http://embed\.ted\.com@', '//embed.ted.com', $link);
+		$link = preg_replace('@^http://www\.youtube\.com@', '//www.youtube.com', $link);
+		$link = preg_replace('@^http://videofarm\.daum\.net@', '//videofarm.daum.net', $link);
+		$link = preg_replace('@^http://embed\.ted\.com@', '//embed.ted.com', $link);
 
-        $this->__setAttr($node, 'src', $link);
-    }
+		$this->__setAttr($node, 'src', $link);
+	}
 
-    private function __node_default($node){
-      $this->__common_attr($node);
-    }
+	private function __node_default($node)
+	{
+		$this->__common_attr($node);
+	}
 }
 
-    // if(php_sapi_name() == "cli"){
-    // 	$html = $argv[1];
-    // 	$xss = new XssHtml($html);
-    // 	$html = $xss->getHtml();
-    // 	echo "'$html'";
-    // }
+// if(php_sapi_name() == "cli"){
+// 	$html = $argv[1];
+// 	$xss = new XssHtml($html);
+// 	$html = $xss->getHtml();
+// 	echo "'$html'";
+// }
 ?>
